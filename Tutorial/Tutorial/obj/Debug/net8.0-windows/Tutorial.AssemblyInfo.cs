2df69f0809735d88d60ff5b66ac3a508056482cb
PJH <nue66360@gmail.com>
2023-12-07 22:38:26 +0900

    페이지 카운트 시작값 수정

--- Tutorial/Tutorial/obj/Debug/net8.0-windows/Tutorial.AssemblyInfo.cs
+++ Tutorial/Tutorial/obj/Debug/net8.0-windows/Tutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e569676da4dee71487675fbbc7bf0b887de9c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5667abe02afff6cb9a34460e0a662e5c22bea7b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
